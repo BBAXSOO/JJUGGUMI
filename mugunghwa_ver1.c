e97ed9ea140510ece6e2d6e8f7a6944260d78bcb
BBAXSOO <145780219+BBAXSOO@users.noreply.github.com>
2023-10-17 00:09:59 +0900

    Add files via upload

--- mugunghwa_ver1.c
+++ mugunghwa_ver1.c
@@ -10,6 +10,7 @@ void gotoxy(int row, int col);
 void draw(void);
 int map_tick = 0;
 int tick = 0;
+int move_tick = 0;
 char map[100][300], front[100][300];
 
 void gotoxy(int row, int col) {
@@ -44,48 +45,46 @@ void draw(void) {
 int main(void) {
     srand(time(NULL));
 
-   
-
-    
-    
-
-    //벽이랑 술래
+    //벽이랑 술래 전환
     while (1) {
-        
+
         for (int i = 0; i < 11; i++) {
             
             for (int j = 0; j < 45; j++) {
-                
+               
                 if (i == 0 || i == 10 || j == 0 || j == 44) {
                     map[i][j] = '#';
                     
                 }
-
-                else if (i >= 4 && i <= 6  && j >=0 && j <= 1 )
+                else if (i >= 4 && i <= 6  && j <= 1 )
                 {   
-                   
-                    if (map_tick >= 10000 && map_tick > 13000)
-                    {
-                        map[i][j] = '@';
-                    }
-                    else 
-                    {
-                        map[i][j] = '#';
-                    }
-                    if (map_tick >= 13000)
-                    {
-                        map_tick = 0;
-                    }
-                    
-                    
+                        if (map_tick >= 10000 && map_tick <= 13000)
+                        {
+                             map[i][j] = '@';
+                        }
+                        else
+                        {
+                             map[i][j] = '#';
+                        }
                 }
                 else
                 {
                     map[i][j] = ' ';
                 }
 
+                if (map_tick <= 13000)
+                {
+                    map_tick += 1000;
+                }
+                else
+                {
+                    if (map_tick > 13000)
+                    {
+                        map_tick == 0;
+                    }
+                }
+                
             }
-
             for (int i = 0; i < 11; i++)
             {
                 for (int j = 0; j < 45; j++)
@@ -96,15 +95,12 @@ int main(void) {
                 }
             }
 
-           
             draw();
-           
-            map_tick += 1000;
+
+
+
+
         }
-
-
-
-
 
 
         //무궁화 꽃이 피었습니다.
@@ -117,27 +113,30 @@ int main(void) {
             }
         }
 
-
         //플레이어 상태창 출력
 
+        printf("\n");
+        char alive[100], dead[100];
         for (int i = 13; i < 14; i++)
         {
             for (int j = 1; j <= 1; j++)
-            {
+            {   
                 printf("\n");
                 printf("no. of players left : 5\n");
-                printf("0Player : %s\n");
-                printf("0Player : %s\n");
-                printf("0Player : %s\n");
-                printf("0Player : %s\n");
-                printf("0Player : %s\n");
-                printf("%d", map_tick);
+                
+                    for (int i = 0; i < 5; i++)
+                    {
+                        printf("%d player : alive\n", i);
+                    }
 
+                    printf("%d", map_tick);
             }
         }
 
 
 
+
+        //움직이는거
         int dx[4] = { -1, 1, 0, 0 };
         int dy[4] = { 0, 0, -1, 1 };
         int x = 3, y = 42;
@@ -146,18 +145,16 @@ int main(void) {
         int playerx[4];
         int playery[4];
 
+
+
         //랜덤하게 움직이는 것들의 위치
         for (int i = 0; i < 4; i++)
         {
-
-            playerx[i] = x + 1 + i;
-            playery[i] = y;
+            playerx[i] = 4 + i;
+            playery[i] = 43;
             map[playerx[i]][playery[i]] = '1' + i;
 
         }
-
-
-
 
         //입력 받아서 움직임
         while (1) {
@@ -185,98 +182,126 @@ int main(void) {
 
 
             //플레이어 이동
-            if (tick % 80 == 0) {
-                int nx = x + dx[dir];
-                int ny = y + dy[dir];
+
+            
+            if (move_tick % 10000 == 0)
+            {
+                
+
+
+                if (tick % 50 == 0) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
 
 
 
-                if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44) {
-                    if (map[nx][ny] != '1' && map[nx][ny] != '2'
-                        && map[nx][ny] != '3' && map[nx][ny] != '4') {
-                        map[x][y] = ' ';
-                        x = nx;
-                        y = ny;
-                        map[x][y] = '0';
+                    if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44) {
+                        if (map[nx][ny] != '1' && map[nx][ny] != '2'
+                            && map[nx][ny] != '3' && map[nx][ny] != '4') {
+                            map[x][y] = ' ';
+                            x = nx;
+                            y = ny;
+                            map[x][y] = '0';
+
+                            if (ny == 1)
+                            {
+                                map[x][y] = ' ';
+                            }
+
+                        }
                     }
+
+
+                    for (int i = 0; i < 4; i++) {
+
+                        if (nx < 1) {
+                            // 왼쪽 벽과 충돌하는 경우 처리
+                            map[playerx[i]][playery[i]] = ' '; // 해당 문자를 공백 문자로 변경하여 화면에서 사라지게 함
+                        }
+                        else {
+                            map[playerx[i]][playery[i]] = '1' + i; // 그 외의 경우에는 원래 문자를 유지
+                        }
+                        int randommove = rand() % 10 + 1;
+                        int randommove_up = rand() % 10 + 1;
+                        int randommove_down = rand() % 10 + 1;
+                        int randommove_stay = rand() % 10 + 1;
+
+
+                        if (randommove <= 7)
+                        {
+                            int nx = playerx[i];
+                            int ny = playery[i] - 1;
+
+                            if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
+                                map[nx][ny] != '1' && map[nx][ny] != '2'
+                                && map[nx][ny] != '3' && map[nx][ny] != '4') {
+                                map[playerx[i]][playery[i]] = ' ';
+                                playerx[i] = nx;
+                                playery[i] = ny;
+                                map[playerx[i]][playery[i]] = '1' + i;
+                            }
+                        }
+
+
+                        if (randommove_up <= 1)
+                        {
+                            int nx = playerx[i] - 1;
+                            int ny = playery[i];
+
+                            if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
+                                map[nx][ny] != '1' && map[nx][ny] != '2'
+                                && map[nx][ny] != '3' && map[nx][ny] != '4') {
+                                map[playerx[i]][playery[i]] = ' ';
+                                playerx[i] = nx;
+                                playery[i] = ny;
+                                map[playerx[i]][playery[i]] = '1' + i;
+                            }
+                        }
+
+
+                        if (randommove_down <= 1)
+                        {
+                            int nx = playerx[i] + 1;
+                            int ny = playery[i];
+
+                            if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
+                                map[nx][ny] != '1' && map[nx][ny] != '2'
+                                && map[nx][ny] != '3' && map[nx][ny] != '4') {
+                                map[playerx[i]][playery[i]] = ' ';
+                                playerx[i] = nx;
+                                playery[i] = ny;
+                                map[playerx[i]][playery[i]] = '1' + i;
+                            }
+                        }
+
+                        if (randommove_stay <= 1)
+                        {
+                            int nx = playerx[i];
+                            int ny = playery[i];
+
+                            if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
+                                map[nx][ny] != '1' && map[nx][ny] != '2'
+                                && map[nx][ny] != '3' && map[nx][ny] != '4') {
+                                map[playerx[i]][playery[i]] = ' ';
+                                playerx[i] = nx;
+                                playery[i] = ny;
+                                map[playerx[i]][playery[i]] = '1' + i;
+                            }
+                        }
+
+                        if (map[nx] == '1' && map[nx] == '2'
+                            && map[nx] == '3' && map[nx] == '4')
+                        {
+                            int ny = playery[i] - 1;
+                        }
+
+
+                    }
+                    
                 }
-
-
-                for (int i = 0; i < 4; i++) {
-                    int randommove = rand() % 10 + 1;
-                    int randommove_up = rand() % 10 + 1;
-                    int randommove_down = rand() % 10 + 1;
-                    int randommove_stay = rand() % 10 + 1;
-                    if (randommove <= 7)
-                    {
-                        int nx = playerx[i];
-                        int ny = playery[i] - 1;
-
-                        if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
-                            map[nx][ny] != '1' && map[nx][ny] != '2'
-                            && map[nx][ny] != '3' && map[nx][ny] != '4') {
-                            map[playerx[i]][playery[i]] = ' ';
-                            playerx[i] = nx;
-                            playery[i] = ny;
-                            map[playerx[i]][playery[i]] = '1' + i;
-                        }
-                    }
-
-
-                    if (randommove_up <= 1)
-                    {
-                        int nx = playerx[i] - 1;
-                        int ny = playery[i];
-
-                        if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
-                            map[nx][ny] != '1' && map[nx][ny] != '2'
-                            && map[nx][ny] != '3' && map[nx][ny] != '4') {
-                            map[playerx[i]][playery[i]] = ' ';
-                            playerx[i] = nx;
-                            playery[i] = ny;
-                            map[playerx[i]][playery[i]] = '1' + i;
-                        }
-                    }
-
-
-                    if (randommove_down <= 1)
-                    {
-                        int nx = playerx[i] + 1;
-                        int ny = playery[i];
-
-                        if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
-                            map[nx][ny] != '1' && map[nx][ny] != '2'
-                            && map[nx][ny] != '3' && map[nx][ny] != '4') {
-                            map[playerx[i]][playery[i]] = ' ';
-                            playerx[i] = nx;
-                            playery[i] = ny;
-                            map[playerx[i]][playery[i]] = '1' + i;
-                        }
-                    }
-
-                    if (randommove_stay <= 1)
-                    {
-                        int nx = playerx[i];
-                        int ny = playery[i];
-
-                        if (nx >= 1 && nx < 10 && ny >= 1 && ny < 44 &&
-                            map[nx][ny] != '1' && map[nx][ny] != '2'
-                            && map[nx][ny] != '3' && map[nx][ny] != '4') {
-                            map[playerx[i]][playery[i]] = ' ';
-                            playerx[i] = nx;
-                            playery[i] = ny;
-                            map[playerx[i]][playery[i]] = '1' + i;
-                        }
-                    }
-
-                    if (map[nx] == '1' && map[nx] == '2'
-                        && map[nx] == '3' && map[nx] == '4')
-                    {
-                        int ny = playery[i] - 1;
-                    }
-
-                }
+                
             }
+
 
 
             draw();
